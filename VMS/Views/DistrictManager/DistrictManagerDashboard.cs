9f4ba911441db44b1007771e5e73568bef9d9b6a
Ashik Rahman <ashrhmn99@gmail.com>
2021-08-20 04:33:06 +0600

    Cleanup Comments

--- VMS/Views/DistrictManager/DistrictManagerDashboard.cs
+++ VMS/Views/DistrictManager/DistrictManagerDashboard.cs
@@ -8,57 +8,15 @@ namespace VMS.Views.DistrictManager
 {
     public partial class DistrictManagerDashboard : Form
     {
-        //private readonly UserDetailRepo _udr;
-        //private readonly CandidateRepo _cr;
         private readonly string _username;
 
         private Form _activeForm;
-        //private Panel mainPanel;
         public DistrictManagerDashboard(string username)
         {
             InitializeComponent();
             _username = username;
             ActivatePanel(new GeneralPublicDashboard(_username));
-            //_udr = new UserDetailRepo();
-            //_cr = new CandidateRepo();
-
-            //this.mainPanel = panelView;
-
-            //RefreshTable();
-
-            //buttonRemoveCandidate.Enabled = false;
-            //buttonAddCandidate.Enabled = false;
         }
-
-        //void RefreshTable()
-        //{
-        //    dataGridViewPublic.DataSource = _cr.GetCandidateDataTable();
-        //    dataGridViewPublic.Columns[1].HeaderText = @"Candidate";
-        //}
-
-        //void UpdateFields(UserDetail userDetail)
-        //{
-        //    if (userDetail != null)
-        //    {
-        //        textBoxName.Text = userDetail.Name;
-        //        textBoxFatherName.Text = userDetail.FatherName;
-        //        textBoxMotherName.Text = userDetail.MotherName;
-        //        textBoxGender.Text = userDetail.Gender;
-        //        textBoxDateOfBirth.Text = userDetail.DateOfBirth.ToString("d");
-        //        textBoxAddress.Text = userDetail.Address;
-        //        textBoxNid.Text = userDetail.Nid;
-        //    }
-        //    else
-        //    {
-        //        textBoxName.Text = "";
-        //        textBoxFatherName.Text = "";
-        //        textBoxMotherName.Text = "";
-        //        textBoxGender.Text = "";
-        //        textBoxDateOfBirth.Text = "";
-        //        textBoxAddress.Text = "";
-        //        textBoxNid.Text = "";
-        //    }
-        //}
 
         void ActivatePanel(Form formToActivate)
         {
@@ -76,53 +34,14 @@ namespace VMS.Views.DistrictManager
             formToActivate.Show();
         }
 
-        private void dataGridViewPublic_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            //if (dataGridViewPublic.SelectedRows[0].Cells[1].Value.ToString() == "Yes")
-            //{
-            //    buttonRemoveCandidate.Enabled = true;
-            //    buttonAddCandidate.Enabled = false;
-            //}
-            //else
-            //{
-            //    buttonRemoveCandidate.Enabled = false;
-            //    buttonAddCandidate.Enabled = true;
-            //}
-
-            //UpdateFields(_udr.GetUserDetail((dataGridViewPublic.SelectedRows[0].Cells[0].Value.ToString())));
-
-        }
-
-        private void buttonAddCandidate_Click(object sender, EventArgs e)
-        {
-            //MessageBox.Show(_cr.SetUserAsCandidate(dataGridViewPublic.SelectedRows[0].Cells[0].Value.ToString())
-            //    ? @"User successfully set as Election Candidate"
-            //    : @"Operation Failed");
-            //RefreshTable();
-        }
-
-        private void buttonRemoveCandidate_Click(object sender, EventArgs e)
-        {
-            //if (_cr.RemoveUserFromCandidate(dataGridViewPublic.SelectedRows[0].Cells[0].Value.ToString()))
-            //{
-            //    MessageBox.Show(@"User successfully removed from Election Candidate");
-            //}
-            //else
-            //{
-            //    MessageBox.Show(@"Operation Failed");
-            //}
-            //RefreshTable();
-        }
 
         private void buttonViewResult_Click(object sender, EventArgs e)
         {
-            //new VoteResult().Show();
             ActivatePanel(new VoteResult());
         }
 
         private void buttonGoToPersonalProfile_Click(object sender, EventArgs e)
         {
-            //new GeneralPublicDashboard(_username).Show();
             ActivatePanel(new GeneralPublicDashboard(_username));
         }
 
