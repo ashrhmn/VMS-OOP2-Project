6ee195a73cc1b0afa7a234c37036bf13eb00e41d
Ashik Rahman <ashrhmn99@gmail.com>
2021-08-20 18:22:46 +0600

    Ad Stage

--- VMS/Views/Admin/AdminDashboard.cs
+++ VMS/Views/Admin/AdminDashboard.cs
@@ -1,177 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
-using VMS.Entity;
-using VMS.Repository;
 
 namespace VMS.Views.Admin
 {
     public partial class AdminDashboard : Form
     {
-        private DataTable _usersDataTable;
-        readonly UserRepo _ur;
-        readonly LocalRepo _lr;
+        private Form _activeForm;
+
         public AdminDashboard()
         {
             InitializeComponent();
-            _ur = new UserRepo();
-            _lr = new LocalRepo();
-            comboBoxOperationMode.SelectedIndex = 0;
-            comboBoxRole.SelectedIndex = 0;
-            UpdateDataTable();
-            userNameErrorLabel.Text = "";
+            ActivateLandingPage();
         }
 
-        public void UpdateDataTable()
+        void ActivateLandingPage()
         {
-            _usersDataTable = _ur.GetUsersDataTable();
-            usersGridView.DataSource = _usersDataTable;
+            ActivatePanel(new AdminPanel());
+            buttonAdminPanel.BackColor = Color.Azure;
+            buttonViewResult.BackColor = Color.LightGray;
         }
 
-        private void AdminDashboard_Load(object sender, EventArgs e)
+        void ActivatePanel(Form formToActivate)
         {
-            UpdateDataTable();
-        }
-
-        private void buttonRefresh_Click(object sender, EventArgs e)
-        {
-            UpdateDataTable();
-        }
-
-
-        public void FetchSelectedDataToTextBoxes()
-        {
-            textBoxUsername.Text = usersGridView.SelectedRows[0].Cells[0].Value.ToString();
-            textBoxPassword.Text = usersGridView.SelectedRows[0].Cells[1].Value.ToString();
-            comboBoxRole.SelectedIndex = comboBoxRole.FindStringExact(usersGridView.SelectedRows[0].Cells[2].Value.ToString());
-        }
-
-        public void EmptyTextBoxes()
-        {
-            textBoxUsername.Text = "";
-            textBoxPassword.Text = "";
-            comboBoxRole.Text = "";
-        }
-
-        private void usersGridView_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            FetchSelectedDataToTextBoxes();
-        }
-
-        private void buttonUpdate_Click(object sender, EventArgs e)
-        {
-
-            if (textBoxUsername.Text == "")
+            if (_activeForm != null)
             {
-                MessageBox.Show(@"Username can not be empty");
+                _activeForm.Close();
             }
-            else
-            {
-                if (comboBoxOperationMode.SelectedIndex == 0)
-                {
-                    //Update
-                    MessageBox.Show(_ur.UpdateUser(
-                        new User(
-                            textBoxUsername.Text,
-                            textBoxPassword.Text,
-                            comboBoxRole.Text
-                        ),
-                        usersGridView.SelectedRows[0].Cells[0].Value.ToString() //username from selected row
-                    )
-                        ? @"Updated successfully"
-                        : @"Update Failed");
-                }
-                else
-                {
-                    //Add
-                    MessageBox.Show(_ur.AddUser(new User(
-                        textBoxUsername.Text,
-                        textBoxPassword.Text,
-                        comboBoxRole.Text
-                    ))
-                        ? @"User added successfully"
-                        : @"User add Failed");
-                }
-
-                EmptyTextBoxes();
-                UpdateDataTable();
-            }
-            
+            _activeForm = formToActivate;
+            formToActivate.TopLevel = false;
+            formToActivate.FormBorderStyle = FormBorderStyle.None;
+            formToActivate.Dock = DockStyle.Fill;
+            panelMainView.Controls.Add(formToActivate);
+            panelMainView.Tag = formToActivate;
+            formToActivate.BringToFront();
+            formToActivate.Show();
         }
 
-
-        private void comboBoxOperationMode_SelectedIndexChanged(object sender, EventArgs e)
+        private void buttonAdminPanel_Click(object sender, EventArgs e)
         {
-            if (comboBoxOperationMode.SelectedIndex == 0)
-            {
-                //Update Mode
-                buttonUpdate.Text = @"Update";
-                buttonDelete.Enabled = true;
-            }
-            else
-            {
-                //Add Mode
-                buttonUpdate.Text = @"Add";
-                buttonDelete.Enabled = false;
-            }
-            CheckUsernameAvailability();
-        }
-
-        private void buttonDelete_Click(object sender, EventArgs e)
-        {
-            if (textBoxUsername.Text == "")
-            {
-                MessageBox.Show(@"Select a user to be deleted", @"Error");
-            }
-            else
-            {
-                var confirmResult = MessageBox.Show(@"Are you sure to delete this item ?",
-                                     @"Confirm Delete!",
-                                     MessageBoxButtons.YesNo);
-                if (confirmResult == DialogResult.Yes)
-                {
-                    MessageBox.Show(_ur.DeleteUser(textBoxUsername.Text)
-                        ? @"User deleted successfully"
-                        : @"Delete Error");
-                }
-                EmptyTextBoxes();
-                UpdateDataTable();
-            }
-        }
-
-        void CheckUsernameAvailability()
-        {
-            if (comboBoxOperationMode.SelectedIndex == 1)
-            {
-                foreach (User user in _lr.ConvertDataTableToList<User>(_usersDataTable).ToArray())
-                {
-                    if (user.Username == textBoxUsername.Text)
-                    {
-                        userNameErrorLabel.Text = @"❌";
-                        userNameErrorLabel.ForeColor = Color.OrangeRed;
-                        textBoxUsername.ForeColor = Color.OrangeRed;
-                        buttonUpdate.Enabled = false;
-
-                        break;
-                    }
-
-                    userNameErrorLabel.Text = textBoxUsername.Text == "" ? "" : @"✅";
-                    userNameErrorLabel.ForeColor = Color.LimeGreen;
-                    textBoxUsername.ForeColor = Color.Black;
-                    buttonUpdate.Enabled = true;
-                }
-            }
-        }
-
-        private void textBoxUsername_TextChanged(object sender, EventArgs e)
-        {
-            CheckUsernameAvailability();
+            ActivateLandingPage();
         }
 
         private void buttonViewResult_Click(object sender, EventArgs e)
         {
-            new VoteResult().ShowDialog(this);
+            ActivatePanel(new VoteResult());
+            buttonAdminPanel.BackColor = Color.LightGray;
+            buttonViewResult.BackColor = Color.Azure;
         }
     }
 }
---
+++ VMS/Views/Admin/AdminPanel.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace VMS.Views.Admin
+{
+    partial class AdminPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usersGridView = new System.Windows.Forms.DataGridView();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxRole = new System.Windows.Forms.ComboBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.comboBoxOperationMode = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.userNameErrorLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // usersGridView
+            // 
+            this.usersGridView.AllowUserToAddRows = false;
+            this.usersGridView.AllowUserToDeleteRows = false;
+            this.usersGridView.AllowUserToResizeRows = false;
+            this.usersGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.usersGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.usersGridView.Location = new System.Drawing.Point(32, 41);
+            this.usersGridView.MultiSelect = false;
+            this.usersGridView.Name = "usersGridView";
+            this.usersGridView.ReadOnly = true;
+            this.usersGridView.RowHeadersVisible = false;
+            this.usersGridView.RowTemplate.Height = 25;
+            this.usersGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.usersGridView.Size = new System.Drawing.Size(407, 284);
+            this.usersGridView.TabIndex = 0;
+            this.usersGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.usersGridView_CellClick);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(32, 331);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(560, 128);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(140, 23);
+            this.textBoxUsername.TabIndex = 4;
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(485, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Username : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(560, 164);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(140, 23);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(509, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Role : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBoxRole
+            // 
+            this.comboBoxRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxRole.FormattingEnabled = true;
+            this.comboBoxRole.Items.AddRange(new object[] {
+            "Admin",
+            "District Manager",
+            "General Public"});
+            this.comboBoxRole.Location = new System.Drawing.Point(560, 198);
+            this.comboBoxRole.Name = "comboBoxRole";
+            this.comboBoxRole.Size = new System.Drawing.Size(140, 23);
+            this.comboBoxRole.TabIndex = 6;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(550, 251);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(631, 251);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 8;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // comboBoxOperationMode
+            // 
+            this.comboBoxOperationMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOperationMode.FormattingEnabled = true;
+            this.comboBoxOperationMode.Items.AddRange(new object[] {
+            "Update Existing",
+            "Add New"});
+            this.comboBoxOperationMode.Location = new System.Drawing.Point(560, 99);
+            this.comboBoxOperationMode.Name = "comboBoxOperationMode";
+            this.comboBoxOperationMode.Size = new System.Drawing.Size(140, 23);
+            this.comboBoxOperationMode.TabIndex = 11;
+            this.comboBoxOperationMode.SelectedIndexChanged += new System.EventHandler(this.comboBoxOperationMode_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Opration Mode :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // userNameErrorLabel
+            // 
+            this.userNameErrorLabel.AutoSize = true;
+            this.userNameErrorLabel.Location = new System.Drawing.Point(706, 131);
+            this.userNameErrorLabel.Name = "userNameErrorLabel";
+            this.userNameErrorLabel.Size = new System.Drawing.Size(44, 15);
+            this.userNameErrorLabel.TabIndex = 13;
+            this.userNameErrorLabel.Text = "ErrMsg";
+            // 
+            // AdminPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 395);
+            this.Controls.Add(this.userNameErrorLabel);
+            this.Controls.Add(this.comboBoxOperationMode);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.comboBoxRole);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.usersGridView);
+            this.Name = "AdminPanel";
+            this.Text = "AdminDashboard";
+            this.Load += new System.EventHandler(this.AdminDashboard_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.usersGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView usersGridView;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxRole;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.ComboBox comboBoxOperationMode;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label userNameErrorLabel;
+    }
+}
